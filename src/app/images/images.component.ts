0adbf44148c8a94f75b7d81b8e9c86594d39382e
unknown <pini7671@gmail.com>
2020-06-14 17:54:09 +0300

    fix-the-logic-buttons

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -7,13 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImagesComponent implements OnInit {
 
+  images: any[] = ["../../assets/pics/1.jpg","../../assets/pics/2.jpg","../../assets/pics/3.jpg"];
+  currentImage: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
-  images: any[] = ["../../assets/pics/1.jpg",
-                  "../../assets/pics/2.jpg",
-                  "../../assets/pics/3.jpg"];
+  previousImage() {
+    if (this.currentImage > 0) {
+      this.currentImage--;
+    }
+  }
 
+  nextImage() {
+    if (this.currentImage < this.images.length - 1) {
+      this.currentImage++;
+    }
+  }
 }
