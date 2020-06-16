5cb7fec8402e3bef73faecae636204dc7bfae451
unknown <pini7671@gmail.com>
2020-06-16 11:49:03 +0300

    add-animate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,7 +13,8 @@ import { ImagesComponent } from './images/images.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnChanges} from '@angular/core';
+import { trigger, transition, state, style, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-images',
   templateUrl: './images.component.html',
-  styleUrls: ['./images.component.less']
+  styleUrls: ['./images.component.less'],
+  animations: [
+    trigger('fade', [
+      state('void' ,style({opacity:0})),
+      transition(':enter, :leave', [
+        animate(1000)
+      ])
+    ])
+  ]
 })
+
 export class ImagesComponent implements OnInit {
 
   images: any[] = ["../../assets/pics/1.jpg","../../assets/pics/2.jpg","../../assets/pics/3.jpg"];
   currentImage: number = 0;
+  newImage: boolean = false;
 
   constructor() { 
    
