9a433a2c37685f3441abf49a4fbca98f8da3e17e
Pini Amiram <pini7671@gmail.com>
2020-06-17 00:09:40 +0300

    add-hover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImagesComponent } from './images/images.component';
 
@@ -12,7 +11,6 @@ import { ImagesComponent } from './images/images.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
